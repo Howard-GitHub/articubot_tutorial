43f314f037884ae097fe3ce6b9b04626bceb2a48
Howard-GitHub <howard.l.general@gmail.com>
2025-07-28 14:31:12 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'articubot_one'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+        (os.path.join('share', package_name, 'description'), glob('description/*')), 
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*')), 
+        (os.path.join('share', package_name, 'config'), glob('config/*'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='howard.l.general@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
