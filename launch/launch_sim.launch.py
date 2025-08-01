0c3bb1458bc9e49842427a73dd3d5a18458e75ee
Howard-GitHub <howard.l.general@gmail.com>
2025-08-01 15:30:19 -0700

    Update

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -65,6 +65,12 @@ def generate_launch_description():
             f'config_file:={bridge_params}',
         ]
     )
+    
+    ros_gz_image_bridge = Node(
+        package="ros_gz_image",
+        executable="image_bridge",
+        arguments=["/camera/image_raw"]
+    )
 
     # Launch them all!
     return LaunchDescription([
@@ -72,5 +78,6 @@ def generate_launch_description():
         rsp,
         gazebo,
         spawn_entity,
-        ros_gz_bridge
+        ros_gz_bridge,
+        ros_gz_image_bridge
     ])
