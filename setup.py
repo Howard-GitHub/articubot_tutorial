0bda23e54329ae50eba95cb4939c2db126b3109d
Howard <150801982+Howard-GitHub@users.noreply.github.com>
2025-09-26 14:53:23 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "pose_subscriber_node = articubot_one.pose_subscriber:main"
         ],
     },
 )
